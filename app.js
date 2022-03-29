00fef01644785c13de09d6a45074cd80d1013fda
JINGWEN LIN <yaysa903@gmail.com>
2022-03-29 21:33:45 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars').create({ defaultLayout: 'main', extname: '.hbs' })
+const methodOverride = require('method-override')
+
+const app = express()
+
+const port = 3000
+
+// express-handlebars
+app.engine('hbs', exphbs.engine)
+app.set('view engine', 'hbs')
+
+// method-override
+app.use(methodOverride('_method'))
+
+// body-parser
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('records')
+})
+
+app.listen(port, () => {
+  console.log(`The server is listening on http://localhost:${port}`)
+})
