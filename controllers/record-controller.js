8257434d129dfa8fc6ffd844bc614c50ba875527
JINGWEN LIN <yaysa903@gmail.com>
2022-03-30 22:35:55 +0800

    feat: add create & edit routes

--- controllers/record-controller.js
+++ controllers/record-controller.js
@@ -1,7 +1,28 @@
+const Record = require('../models/Record')
+const Category = require('../models/Category')
+
 const recordController = {
-  getRecords: (req, res) => {
-    res.render('records')
-  }
+  getRecords: async (req, res) => {
+    try {
+      res.render('records')
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  getCreatePage: async (req, res) => {
+    try {
+      res.render('create-records')
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  getEditPage: async (req, res) => {
+    try {
+      res.render('edit-records')
+    } catch (err) {
+      console.log(err)
+    }
+  },
 }
 
 module.exports = recordController
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const recordController = require('../../controllers/record-controller')
 
 router.get('/', recordController.getRecords)
+router.get('/create', recordController.getCreatePage)
+router.get('/:id/edit', recordController.getEditPage)
 
 module.exports = router
