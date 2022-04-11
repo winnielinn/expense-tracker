0599d3ca29499dc633f42729162d124fbb0152e2
JINGWEN LIN <yaysa903@gmail.com>
2022-04-11 23:31:17 +0800

    feat: add thousands separators in amount

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,8 @@
 module.exports = {
   compareObjectID: (category, record, options) => {
     return category._id.toString() === record.categoryId.toString() ? options.fn(this) : options.inverse(this)
+  },
+  setAmount: (amount) => {
+    return amount.toLocaleString()
   }
 }
