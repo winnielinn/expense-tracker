cf27236b64565a76d35f7e1bb1b3c40e73e8f260
JINGWEN LIN <yaysa903@gmail.com>
2022-04-26 20:18:18 +0800

    feat: add nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+module.exports = (user, email, verifyCode) => {
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.USER_ACCOUNT,
+      pass: process.env.USER_PASS
+    },
+  })
+
+  let mailOptions = {
+    from: process.env.USER_ACCOUNT,
+    to: email,
+    subject: '家庭記帳本-重設密碼',
+    html: `
+          <p>${user.name} 您好,</p>
+          <p>請回到忘記密碼頁面後輸入 <strong>${verifyCode}</strong> 進行重設密碼的動作。</p>
+        `
+    ,
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('訊息發送: ' + info.response)
+    }
+  })
+}
+
+
+
+
